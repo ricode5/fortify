7259565ba9a562aa2ad1a5c743d1c040d487d916
Riccardo Serio <riccardoserio46@gmail.com>
2023-05-31 19:18:49 +0200

    Scaffolding completo

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <x-navbar />
+
+    <div class="min-vh-100 container">
+        {{$slot}}
+    </div>
+
+    <x-footer />
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+
+    <header class="container-fluid p-5">
+        <div class="row justify-content center">
+            <div class="col 12">
+                <h1 class="display-1 text-center">Benvenuti HKTP 6</h1>
+            </div>
+        </div>
+    </header>
+
+</x-layout>
